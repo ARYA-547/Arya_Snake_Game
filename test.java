ec0ecb2ccf303d329a53d0bd7a7d2d187f6d2bce
Deepak Nayak <aryakumarbhanja9@gmail.com>
2024-06-13 13:48:22 +0530

    Arya Snake Game

---
+++ test.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+class test {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 2 numbers:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Sum of " + a + " and " + b + " =" + a + b);
+    }
+}
